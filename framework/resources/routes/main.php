5d8db148ac9f7ee9e212588a7f34058fdd3daad1
Foma Tuturov <fomiash@yandex.ru>
2021-02-02 23:15:59 +0300

    upd origin route

--- framework/resources/routes/main.php
+++ framework/resources/routes/main.php
@@ -1,10 +1,4 @@
 <?php
-/**
-Route::before('<before>Before@index');
-Route::get("/{test}/{main}/{before}/");
-Route::controller('<test>@get<main>');
- * **/
-
 
 Route::get("/", "MAIN-PAGE");
 
@@ -28,11 +22,11 @@ Route::type("post")->get("/test-8/", "TEST-MAIN-TYPE-POST");
 
 Route::getType('post');
 
-Route::get("/test-9/1/", "TEST-GET-TYPE-POST-1"); // POST
+    Route::get("/test-9/1/", "TEST-GET-TYPE-POST-1"); // POST
 
-Route::type('get')->get("/test-9/2/", "TEST-GET-TYPE-POST-2"); // GET
+    Route::type('get')->get("/test-9/2/", "TEST-GET-TYPE-POST-2"); // GET
 
-Route::get("/test-9/3/", "TEST-GET-TYPE-POST-3"); // POST
+    Route::get("/test-9/3/", "TEST-GET-TYPE-POST-3"); // POST
 
 Route::endType();
 
@@ -40,23 +34,23 @@ Route::type('post')->type('get')->get("/test-9/4/", "TEST-GET-TYPE-POST-4"); // 
 
 Route::prefix("/test-10-prefix/")->getGroup(); // Начало группы
 
-Route::get("/test-10/1/", "TEST-MAIN-GROUP-1");
+    Route::get("/test-10/1/", "TEST-MAIN-GROUP-1");
 
-Route::get("/test-10/2/", "TEST-MAIN-GROUP-2");
+    Route::get("/test-10/2/", "TEST-MAIN-GROUP-2");
 
 Route::endGroup(); // Завершение группы
 
 Route::prefix("/test-11-prefix/")->getGroup(); // Начало группы 1
 
-Route::prefix("/test-11-prefix-2/")->getGroup(); // Начало группы 2
+    Route::prefix("/test-11-prefix-2/")->getGroup(); // Начало группы 2
 
-Route::get("/test-11-group/1/", "TEST-IN-GROUP-1");
+        Route::get("/test-11-group/1/", "TEST-IN-GROUP-1");
 
-Route::get("/test-11-group/2/", "TEST-IN-GROUP-2");
+        Route::get("/test-11-group/2/", "TEST-IN-GROUP-2");
 
-Route::endGroup(); // Завершение группы 2
+    Route::endGroup(); // Завершение группы 2
 
-Route::get("/test-11-group/3/", "TEST-IN-GROUP-3");
+    Route::get("/test-11-group/3/", "TEST-IN-GROUP-3");
 
 Route::endGroup(); // Завершение группы 1
 
@@ -67,9 +61,9 @@ Route::prefix('/test-12-2/');
 
 Route::getGroup(); // Начало группы 2
 
-Route::get('/a3/', "TEST-NAMED-GROUP-1"); // "/test-12-1/test-12-2/a3/"
+    Route::get('/a3/', "TEST-NAMED-GROUP-1"); // "/test-12-1/test-12-2/a3/"
 
-Route::get('/b3/', "TEST-NAMED-GROUP-2"); // "/test-12-1/test-12-2/b3/"
+    Route::get('/b3/', "TEST-NAMED-GROUP-2"); // "/test-12-1/test-12-2/b3/"
 
 Route::endGroup('Group 1'); // Завершение группы "Group 1"
 
@@ -77,26 +71,26 @@ Route::get('/test-12-3/', "TEST-NAMED-GROUP-3"); // "/test-12-2/test-12-3/"
 
 Route::endGroup(); // Завершение группы 2
 
-Route::protect()->get('/test-13-pro/', 'TEST-PROTECTED-1');
+    Route::protect()->get('/test-13-pro/', 'TEST-PROTECTED-1');
 
-Route::getProtect();
+    Route::getProtect();
 
-Route::get('/test-14/1/', "TEST-GET-PROTECT-1"); // Защищён
+    Route::get('/test-14/1/', "TEST-GET-PROTECT-1"); // Защищён
 
-Route::protect('None')->get('/test-14/2/', "TEST-GET-PROTECT-2"); // Не защищён
+    Route::protect('None')->get('/test-14/2/', "TEST-GET-PROTECT-2"); // Не защищён
 
-Route::get('/test-14/3/', "TEST-GET-PROTECT-3"); // Защищён
+    Route::get('/test-14/3/', "TEST-GET-PROTECT-3"); // Защищён
 
-Route::endProtect();
+    Route::endProtect();
 
-// Работа с поддоменами
-Route::prefix("/test-subdomains/")->getGroup();
+    // Работа с поддоменами
+    Route::prefix("/test-subdomains/")->getGroup();
 
-Route::domain(null)->get('/default/', "TEST-SUBDOMAIN-DEFAULT");
+        Route::domain(null)->get('/default/', "TEST-SUBDOMAIN-DEFAULT");
 
-Route::domain("dev")->get('/default/', "TEST-SUBDOMAIN-DEV-1");
+        Route::domain("dev")->get('/default/', "TEST-SUBDOMAIN-DEV-1");
 
-Route::endGroup();
+    Route::endGroup();
 
 Route::domain("dev1")->getGroup();
 
@@ -131,25 +125,25 @@ $middlewareBeforeName = 'Test6032ca1ff49c11e8a7a2b17afed6858bMiddlewareBefore';
 
 Route::prefix('test-controller')->getGroup();
 
-Route::get('/cnt0/')->controller($controllerName);
+    Route::get('/cnt0/')->controller($controllerName);
 
-Route::get('/cnt1/')->controller($controllerName . '@getTestVariables', ['testValue1', 'testValue2']);
+    Route::get('/cnt1/')->controller($controllerName . '@getTestVariables', ['testValue1', 'testValue2']);
 
-Route::get('/cnt2/')->controller($controllerName . '@getTestVariables', ['testValue1', 100500]);
+    Route::get('/cnt2/')->controller($controllerName . '@getTestVariables', ['testValue1', 100500]);
 
-Route::get('/cnt3/')->controller($controllerName)->after($middlewareAfterName);
+    Route::get('/cnt3/')->controller($controllerName)->after($middlewareAfterName);
 
-Route::get('/cnt4/')->controller($controllerName)->after($middlewareAfterName . '@getTestParams', ['testMValue1', 'testMValue2']);
+    Route::get('/cnt4/')->controller($controllerName)->after($middlewareAfterName . '@getTestParams', ['testMValue1', 'testMValue2']);
 
-Route::get('/cnt5/', "MAIN-PAGE")->after($middlewareAfterName . '@getTestParams', ['testMValue1', 'testMValue2']);
+    Route::get('/cnt5/', "MAIN-PAGE")->after($middlewareAfterName . '@getTestParams', ['testMValue1', 'testMValue2']);
 
-Route::get('/cnt6/', "MAIN-PAGE")->after($middlewareAfterName . '@getTestParams', ['testMValue1', 100500]);
+    Route::get('/cnt6/', "MAIN-PAGE")->after($middlewareAfterName . '@getTestParams', ['testMValue1', 100500]);
 
-Route::before($middlewareBeforeName)->before($middlewareBeforeName)->get('/cnt7/', "MAIN-PAGE")->after($middlewareAfterName)->after($middlewareAfterName);
+    Route::before($middlewareBeforeName)->before($middlewareBeforeName)->get('/cnt7/', "MAIN-PAGE")->after($middlewareAfterName)->after($middlewareAfterName);
 
-Route::before($middlewareBeforeName)->before($middlewareBeforeName . '@getTestParams', ['testMbValue1', 'testMbValue2'])->get('/cnt8/', "MAIN-PAGE");
+    Route::before($middlewareBeforeName)->before($middlewareBeforeName . '@getTestParams', ['testMbValue1', 'testMbValue2'])->get('/cnt8/', "MAIN-PAGE");
 
-Route::before($middlewareBeforeName . '@getTestParams', ['testMbValue11', 'testMbValue21'])->before($middlewareBeforeName . '@getTestParams', ['testMbValue1', 'testMbValue2'])->get('/cnt9/', "MAIN-PAGE");
+    Route::before($middlewareBeforeName . '@getTestParams', ['testMbValue11', 'testMbValue21'])->before($middlewareBeforeName . '@getTestParams', ['testMbValue1', 'testMbValue2'])->get('/cnt9/', "MAIN-PAGE");
 
 Route::endGroup();
 
@@ -166,18 +160,19 @@ $compoundName5 = '<class1>474eff721eee4c33056ab6a4c91b522bNum1<class2>';
 
 Route::prefix('test-compound-controller')->getGroup();
 
-Route::get('/cp0/{test}/')->controller($compoundName1);
+    Route::get('/cp0/{test}/')->controller($compoundName1);
 
-Route::get('/cp1/{main}/{test}/')->controller($compoundName1);
+    Route::get('/cp1/{main}/{test}/')->controller($compoundName1);
 
-Route::get('/cp2/{main}/{test}/')->controller($compoundName2);
+    Route::get('/cp2/{main}/{test}/')->controller($compoundName2);
 
-Route::get('/cp3/{main}/{test}/')->controller($compoundName1 . '@index');
+    Route::get('/cp3/{main}/{test}/')->controller($compoundName1 . '@index');
 
-Route::get('/cp4/{mainx}/{testx}/')->controller($compoundName4);
+    Route::get('/cp4/{mainx}/{testx}/')->controller($compoundName4);
 
-Route::get('/cp5/{test}/{method}/')->controller($compoundName1 . '@<method>');
+    Route::get('/cp5/{test}/{method}/')->controller($compoundName1 . '@<method>');
 
 Route::endGroup();
+
 
 
