74b65287a830f536a7efcfe2b42accd5781db9f2
Foma Tuturov <fomiash@yandex.ru>
2024-08-31 13:05:10 +0300

    test once function

--- system/framework/standard/unit/SeparateFunctionsTest.php
+++ system/framework/standard/unit/SeparateFunctionsTest.php
@@ -16,6 +16,25 @@ class SeparateFunctionsTest extends TestCase
         require_once __DIR__ . '/../functions.php';
     }
 
+    public function testOnceFunction(): void
+    {
+        $counter = 0;
+        $func = function () use (&$counter) {
+            $counter++;
+            return $counter;
+        };
+
+        $once = function(callable $func) {
+            return once($func);
+        };
+
+        $result1 = $once($func);
+        $result2 = $once($func);
+
+        $condition = ($result1 === $result2) && ($counter === 1);
+        $this->assertTrue($condition);
+    }
+
     public function testArrayFindFunction(): void
     {
         $array = [1, 2, 3, 4, 5];
