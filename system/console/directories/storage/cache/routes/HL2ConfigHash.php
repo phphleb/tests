fe410e35b57378a2978cbba79d76da920ce87807
Foma Tuturov <fomiash@yandex.ru>
2024-12-10 14:40:34 +0300

    new version

---
+++ system/console/directories/storage/cache/routes/HL2ConfigHash.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Phphleb\directories\storage\cache\routes;
+/**
+ * This class is generated automatically. It will be changed during the update.
+ *
+ * Этот класс сгенерирован автоматически. Он будет изменён при обновлении.
+ *
+ * @internal
+ */
+final class HL2ConfigHash
+{
+    /** @internal */
+    public const HASH = '20c78c7a0744';
+}
