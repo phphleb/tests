8222e153fa481db0aefcf85b151c1598b883011f
Foma Tuturov <fomiash@yandex.ru>
2025-09-14 20:52:22 +0300

    FIX: tests

--- system/framework/async/runner/tests/ResetHelperTest.php
+++ system/framework/async/runner/tests/ResetHelperTest.php
@@ -16,47 +16,67 @@ class ResetHelperTest extends TestCase
     {
         require_once __DIR__ . '/../standard_autoloader.php';
     }
-    public function testResetService(): void
+
+    public function testStandardResetService(): void
     {
         $item = new ResetItem();
         $item->setData(10);
         $services = [$item];
         ResetAndRollbackHelper::resetServices($services, NULL);
         $this->assertTrue($item->data === 0);
+    }
 
+    public function testStandardResetService2(): void
+    {
         if (PHP_VERSION_ID >= 80400) {
             $lazyObject = CoreContainer::getLazyObject(ResetItem::class);
             $services = [$lazyObject];
             ResetAndRollbackHelper::resetServices($services, NULL);
             $this->assertTrue($lazyObject->data === null);
-
-            $lazyObject->setData(2);
-
-            $services = [$lazyObject];
-            ResetAndRollbackHelper::resetServices($services, NULL);
-            $this->assertTrue($lazyObject->data === 0);
         }
     }
 
-    public function testResetServiceWithConstructor(): void
+    public function testStandardResetService3(): void
+    {
+        if (PHP_VERSION_ID >= 80400) {
+            $lazyObject = CoreContainer::getLazyObject(ResetItem::class);
+            $lazyObject->setData(2);
+            $services = [$lazyObject];
+
+           ResetAndRollbackHelper::resetServices($services, NULL);
+           $this->assertTrue($lazyObject->data === 0);
+        }
+    }
+
+    public function testStandardResetServiceWithConstructor(): void
     {
         $item = new ResetItemWithConstructor(7);
         $item->setData(10);
         $services = [$item];
         ResetAndRollbackHelper::resetServices($services, NULL);
         $this->assertTrue($item->data === 0);
+    }
 
+    public function testStandardResetServiceWithConstructor2(): void
+    {
         $object = CoreContainer::getLazyObject(ResetItemWithConstructor::class, [7]);
         $this->assertTrue($object->data === 7);
+    }
 
-
+    public function testStandardResetServiceWithConstructor3(): void
+    {
         if (PHP_VERSION_ID >= 80400) {
-
             $lazyObject = CoreContainer::getLazyObject(ResetItemWithConstructor::class, [7]);
             $services = [$lazyObject];
             ResetAndRollbackHelper::resetServices($services, NULL);
             $this->assertTrue($lazyObject->data === 0);
+        }
+    }
 
+    public function testStandardResetServiceWithConstructor4(): void
+    {
+        if (PHP_VERSION_ID >= 80400) {
+            $lazyObject = CoreContainer::getLazyObject(ResetItemWithConstructor::class, [7]);
             $lazyObject->setData(2);
 
             $services = [$lazyObject];
